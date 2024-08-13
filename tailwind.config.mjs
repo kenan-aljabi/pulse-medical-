01a1745f04cd09ef2705530c6c63da0674b9695d
kenan-aljabi <kenanaaxx@gmail.com>
2024-08-14 01:13:57 +0300

    new commit

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,14 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'custom-black': '#00000040',
+			},
+			fontFamily: {
+				lexend: ['Lexend', 'sans-serif'],
+			},
+		},
 	},
 	plugins: [],
 }
